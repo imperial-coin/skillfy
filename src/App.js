c00c5644eba8a1e79e2925f67f9f2d6f9e009a3d
Max G <john.withabusiness@gmail.com>
2021-10-11 20:19:33 +0200

    files structured

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+//Screens
+import Header from "./screens/header"
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
---
+++ src/screens/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from '../assets/logo.png';
+
+function Header(){
+    return (
+         
+        <div className="header fixed">                                        
+            <h1>Header</h1>   
+		      
+
+        
+        
+
+        </div>
+    )
+}
+
+export default Header;
